5d42c170deb78dd1c9ab0917c454d96db9700def
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-15 21:23:21 +0700

    feat: finish UI all role

--- MilkShop/Views/Staff/StaffWindow.xaml.cs
+++ MilkShop/Views/Staff/StaffWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MilkShop.Views.Auth;
+using MilkShop.Views.Staff.Control;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,71 @@ namespace MilkShop.Views.Staff
         public StaffWindow()
         {
             InitializeComponent();
+            LV.SelectedIndex = 0;
+        }
+
+        private void LV_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LV.SelectedItem is ListViewItem selectedItem)
+            {
+                switch (selectedItem.Tag.ToString())
+                {
+                    case "Product":
+                        ContentArea.Content = new ProductManagementControl();
+                        break;
+                    case "User":
+                        ContentArea.Content = new UserManagementControl();
+                        break;
+                    case "Blog":
+                        ContentArea.Content = new BlogManagementControl();
+                        break;
+                    case "Order":
+                        ContentArea.Content = new OrderManagementControl();
+                        break;
+                    case "SignOut":
+                        LoginWindow login = new LoginWindow();
+                        login.Show();
+                        this.Close();
+                        break;
+                }
+            }
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_product.Visibility = Visibility.Collapsed;
+                tt_user.Visibility = Visibility.Collapsed;
+                tt_blog.Visibility = Visibility.Collapsed;
+                tt_order.Visibility = Visibility.Collapsed;
+                tt_signout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_product.Visibility = Visibility.Visible;
+                tt_user.Visibility = Visibility.Visible;
+                tt_blog.Visibility = Visibility.Visible;
+                tt_order.Visibility = Visibility.Visible;
+                tt_signout.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
         }
     }
 }
