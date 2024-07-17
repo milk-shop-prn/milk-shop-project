7784a677eed5204ed236a96eb258f2d2a704069d
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-18 01:06:37 +0700

    feat: add dashboard

--- MilkShop/Views/Admin/Control/DashboardControl.xaml.cs
+++ MilkShop/Views/Admin/Control/DashboardControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,29 @@ namespace MilkShop.Views.Admin.Control
     /// </summary>
     public partial class DashboardControl : UserControl
     {
+        IOrderService orderService;
+        IUserService userService;
+        IProductService productService;
         public DashboardControl()
         {
             InitializeComponent();
+            orderService = new OrderService();
+            userService = new UserService();
+            productService = new ProductService();
+            LoadDashboard();
+        }
+
+        private void LoadDashboard()
+        {
+            var totalOrder = orderService.GetAll().Count(order => order.OrderStatus.Equals("Delivered"));
+            var totalReveue = orderService.GetAll().Where(order => order.OrderStatus.Equals("Delivered")).Sum(order => order.TotalPrice);
+            var totalUser = userService.GetAll().Count(user => user.Deleted != true);
+            var totalProduct = productService.GetAll().Count(product => product.Deleted != true);
+
+            BlkOrder.Text = totalOrder.ToString();
+            BlkRevenue.Text = Math.Round(totalReveue).ToString();
+            BlkProduct.Text = totalProduct.ToString();
+            BlkUser.Text = totalUser.ToString();
         }
     }
 }
